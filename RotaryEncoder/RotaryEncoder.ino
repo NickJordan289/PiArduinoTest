7e0ae136ef1641bc5138e5f259e1a7abfc398063
Nicholas Jordan <nickjordan289@gmail.com>
2019-06-21 14:35:40 +0800

    Added 4 Digit display

--- RotaryEncoder/RotaryEncoder.ino
+++ RotaryEncoder/RotaryEncoder.ino
@@ -1,14 +1,22 @@
+#include "TM1637Display.h"
+
+#define CLK 2
+#define DIO 3
+
 #define outputA 6
 #define outputB 7
-#define redLed 10
-#define yelLed 9
+
 #define greLed 8
+#define yelLed 9
+#define redLed 10
 
 int multiplier = 5;
 
 int counter = 50;
 int aState;
 int aLastState;
+
+TM1637Display display(CLK, DIO);
 
 void setup() {
   pinMode(outputA, INPUT);
@@ -23,6 +31,12 @@ void setup() {
   aLastState = digitalRead(outputA);
 
   digitalWrite(yelLed, HIGH); // default to on
+
+  // brightness of our display, can go much higher
+  display.setBrightness(2); 
+  
+  // set default display value
+  display.showNumberDec(counter, false); 
 }
 
 void loop() {
@@ -33,6 +47,9 @@ void loop() {
     } else {
       counter += multiplier;
     }
+    
+    display.showNumberDec(counter, false);
+    
     if(counter < 25) {
       digitalWrite(redLed, HIGH);
       digitalWrite(yelLed, LOW);
